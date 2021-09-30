4bb21f001b573446d15d5db7f81953f45f1e1117
Ediberto Ponce <ediberto01@gmail.com>
2021-09-30 15:46:22 -0700

    delete feature fully functional

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -12,47 +12,67 @@ class BestBooks extends React.Component {
       // displayBooks: false,
     }
   }
-  
+
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
-  
+
   async componentDidMount() {
     const booksURL = process.env.REACT_APP_API_URL + '/books'; // localhost
     //let booksURL = 'https://kroffe-can-of-books.herokuapp.com/books' //heroku
     console.log(booksURL, 'url here?');
 
-    const bookResponse = await axios.get(booksURL);
-    this.setState({books: bookResponse.data});
+    // const bookResponse = await axios.get(booksURL);
+    // this.setState({ books: bookResponse.data });
+    this.getbooks();
+  }
+
+
+  handleDelete = async (id, email) => {
+
+    // let newBooks = this.state.books;
+    // const superNewBooks = newBooks.filter(book => book._id !== id)
+    // this.setState({
+    //   books: superNewBooks
+    // })
+    await axios.delete(`http://localhost:3001/books/${id}?email=${email}`);
+    this.getbooks();
+  };
+  getbooks = async () => {
+    const booksURL = 'http://localhost:3001/books';
+    let booksResponse = await axios.get(booksURL);
+    let bookData = booksResponse.data;
+    this.setState({ books: bookData });
+    console.log('bookData' + booksResponse.data[0].title)
   }
 
 
   render() {
     /* TODO: render user's books in a Carousel */
-    console.log('NEW BOOK',this.props.newBook)
+    console.log('NEW BOOK', this.props.newBook)
     console.log(this.state.books)
     return (
       <>
-      <h2>The Best Books Around</h2> 
-      {this.state.books.length > 0 ? 
-      <Carousel className="d-block w-50">
-        {this.state.books.map((book) => (
-          <Carousel.Item key={book._id}>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/50"
-              alt={book.title}
-            />
-            <Carousel.Caption>
-              <h3>{book.title}</h3> 
-              <p>{book.description}</p> 
-              <Button value="delete" onClick={() => this.props.handleDelete(book._id, book.email)}>Delete this book?</Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
+        <h2>The Best Books Around</h2>
+        {this.state.books.length > 0 ?
+          <Carousel className="d-block w-50">
+            {this.state.books.map((book) => (
+              <Carousel.Item key={book._id}>
+                <img
+                  className="d-block w-100"
+                  src="https://via.placeholder.com/50"
+                  alt={book.title}
+                />
+                <Carousel.Caption>
+                  <h3>{book.title}</h3>
+                  <p>{book.description}</p>
+                  <Button value="delete" onClick={() => this.handleDelete(book._id, book.email)}>Delete this book?</Button>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
 
 
-      </Carousel>
-      : <p>Sorry, no books available.</p>
-      }
+          </Carousel>
+          : <p>Sorry, no books available.</p>
+        }
       </>
     )
   }
